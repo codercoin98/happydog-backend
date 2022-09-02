698a9aaaaab7a1baec52458b753e92a6c7877313
chenbin.li <lichenbin1119@gmail.com>
2022-09-02 17:33:34 +0800

    feature:添加swagger api

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as session from 'express-session';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //配置session
   app.use(
     session({
       secret: 'HappyDog',
@@ -12,6 +14,14 @@ async function bootstrap() {
       cookie: { maxAge: 9999999 },
     }),
   );
+  //使用swagger-api文档
+  const config = new DocumentBuilder()
+    .setTitle('api document for HappyDog')
+    .setDescription('HappyDog api document')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
