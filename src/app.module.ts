48be76a543fd60f8eb9fd052df8695e651d84dc0
chenbin.li <lichenbin1119@gmail.com>
2022-10-08 14:03:44 +0800

    feature:添加评论和回复模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { UploadModule } from './upload/upload.module';
 import { PostModule } from './post/post.module';
+import { CommentModule } from './comment/comment.module';
+import { ReplyModule } from './reply/reply.module';
 @Module({
   imports: [
     AuthModule,
     UserModule,
     PostModule,
     MongooseModule.forRoot('mongodb://localhost:27017/happydog'),
-    UploadModule
+    UploadModule,
+    CommentModule,
+    ReplyModule
   ],
   controllers: [AppController],
   providers: [AppService]
